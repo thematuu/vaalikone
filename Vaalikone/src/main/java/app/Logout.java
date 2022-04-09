4bf2c8c2cb6159bf97c0058229f523665d276cff
TheZahu <75035036+TheZahu@users.noreply.github.com>
2022-04-09 16:20:22 +0300

    Updated java and html files

--- Vaalikone/src/main/java/app/Logout.java
+++ Vaalikone/src/main/java/app/Logout.java
@@ -2,6 +2,8 @@ package app;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,12 +23,11 @@ public class Logout extends HttpServlet {
 			throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-
+		HttpSession session = request.getSession(false);
+		session.removeAttribute("currentSessionUser");
 		request.getRequestDispatcher("admin.html").include(request, response);
 
-		HttpSession session = request.getSession();
-		session.invalidate();
-
+		
 		out.print("You are successfully logged out!");
 
 		out.close();
